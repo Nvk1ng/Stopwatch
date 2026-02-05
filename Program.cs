69bce134436d328daf52f85d1c95a991c36935c8
matheus sanderhus <sanderhus15@outlook.com>
2026-02-05 11:28:43 -0300

    Estrutura do cronometro

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                currentTime ++;
+            }
         }
     }
 }
